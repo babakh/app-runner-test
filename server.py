f182c745a0bbca5a74d1a8ef7eacdfd2e725cb84
babak h <babakh@yahoo.com>
2026-03-22 19:17:53 -0400

    revert

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
-from flask import Flask
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 import os
 
-app = Flask(__name__)
-
-
-@app.get("/")
-def hello_world():
-    name = os.environ.get("NAME", "world").strip() or "world"
-    return f"Hello, {name}!\n"
-
-
-@app.get("/health")
-def health():
-    return "ok\n", 200
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port)
+    # port = int(os.environ.get("PORT"))
+    port = 8080
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
